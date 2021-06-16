695ca3ead8eaa8646138ba6f59365e55bda31f3d
(GitHHaruTanaka828 <tanaka.lifes@gmail.com>
2021-06-16 07:49:45 +0000

    responsive

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,12 +10,14 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
-//= require jquery
 //= require bootstrap
+
+
 
 
 //トップfooterロゴ クリックでスクロール
@@ -63,6 +65,7 @@ jQuery(function($){
     pos = currentPos;//現在地保存
   });
 });
+
 
 //社訓のフェードイン
 
@@ -170,6 +173,33 @@ $(window).scroll(function(){
  });
 });
 
+
+//スキッパー
+
+$(document).ready(function () {
+  $(".images").skippr({
+    // スライドショーの変化 ("fade" or "slide")
+    transition : 'slide',
+    // 変化に係る時間(ミリ秒)
+    speed : 2000,
+    // easingの種類
+    easing : 'easeOutQuart',
+    // ナビゲーションの形("block" or "bubble")
+    navType : 'block',
+    // 子要素の種類("div" or "img")
+    childrenElementType : 'div',
+    // ナビゲーション矢印の表示(trueで表示)
+    arrows : false,
+    // スライドショーの自動再生(falseで自動再生なし)
+    autoPlay : true,
+    // 自動再生時のスライド切替間隔(ミリ秒)
+    autoPlayDuration : 3000,
+    // キーボードの矢印キーによるスライド送りの設定(trueで有効)
+    keyboardOnAlways : true,
+    // 一枚目のスライド表示時に戻る矢印を表示するかどうか(falseで非表示)
+    hidePrevious : false
+  });
+});
 
 
 })
